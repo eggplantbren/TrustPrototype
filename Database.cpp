152964f0b2609a042150f7f6de63ed6eed54266f
Brendon J. Brewer <brendon.brewer@gmail.com>
2021-10-06 18:20:39 +1300

    Databse file

--- Database.cpp
+++ Database.cpp
@@ -1,1 +1,20 @@
 #include "Database.h"
+
+namespace Trust
+{
+
+Database::Database()
+:db("data/trust.db")
+{
+    pragmas();
+    create_tables();
+    create_indices();
+}
+
+void Database::pragmas()
+{
+    
+}
+
+
+} // namespace
--- Database.h
+++ Database.h
@@ -1,12 +1,22 @@
 #ifndef Trust_Database_h
 #define Trust_Database_h
 
+#include "sqlite_modern_cpp/hdr/sqlite_modern_cpp.h"
+
 namespace Trust
 {
 
 class Database
 {
+    private:
+        sqlite::database db;
 
+        void pragmas();
+        void create_tables();
+        void create_indices();
+
+    public:
+        Database();
 
 
 
