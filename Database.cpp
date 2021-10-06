79e19f9df4c5bdd9e6e4c81b91614d879e58171b
Brendon J. Brewer <brendon.brewer@gmail.com>
2021-10-06 18:27:30 +1300

    stuff

--- Database.cpp
+++ Database.cpp
@@ -13,8 +13,32 @@ Database::Database()
 
 void Database::pragmas()
 {
-    
+    db << "PRAGMA FOREIGN_KEYS = OFF;";
+    db << "PRAGMA JOURNAL_MODE = WAL;";
+    db << "PRAGMA SYNCHRONOUS = 0;";
 }
 
+void Database::create_tables()
+{
+    db << "CREATE TABLE IF NOT EXISTS nodes\
+(claim_hash BLOB NOT NULL PRIMARY KEY,\
+ amount     INTEGER NOT NULL,\
+ trust      REAL NOT NULL)\
+WITHOUT ROWID;";
+
+    db << "CREATE TABLE IF NOT EXISTS edges\
+(to     BLOB NOT NULL REFERENCES nodes (claim_hash),\
+ from   BLOB REFERENCES nodes (claim_hash),\
+ amount INTEGER NOT NULL,\
+ trust  REAL NOT NULL)\
+WITHOUT ROWID;";
+
+}
+
+
+void Database::create_indices()
+{
+
+}
 
 } // namespace
--- Makefile
+++ Makefile
@@ -2,6 +2,8 @@ FLAGS = -std=c++20 -O2 -Wall -Wextra -pedantic
 
 default: sqlite3.o
 	g++ $(FLAGS) -c Database.cpp
+	g++ $(FLAGS) -c main.cpp
+	g++ -pthread -o main main.o sqlite3.o Database.o -lpthread
 
 sqlite3.o:
 	gcc -O2 -c sqlite-amalgamation-3360000/sqlite3.c
